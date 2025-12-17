d4068461f32fdb4d7c31f2d39b8db27af60e9f4f
elichiara <chiarottoelisa@gmail.com>
2025-12-17 19:08:47 +0100

    loading test model and print answer to query

--- main.py
+++ main.py
@@ -3,19 +3,29 @@ import torch
 from transformers import pipeline # pipeline is for inference
 
 # check if I have an Nvidia GPU on the machine
-print(torch.cuda.is_available())
+print("Is cuda available?", torch.cuda.is_available())
 #print(torch.cuda.device_count(), torch.cuda.get_device_name(0), sep="\n")
 
 # this will import the LLM
 #model = pipeline(model="codefuse-ai/CodeFuse-CGM-72B")
 
 # importing the model
-model = pipeline("text-generation", model="Qwen/Qwen3-Coder-30B-A3B-Instruct")
+model = pipeline("text-generation", model="Qwen/Qwen2.5-Coder-1.5B-Instruct")
 
-user_prompt = "how to plot sin(x) in python?"
+user_prompt = "how can I plot sin(x) in python?"
 messages = [
     {"role": "user", "content": user_prompt},
 ]
 
-response = model(messages)
-print(response)
+chat_history = model(messages)
+"""
+chat_history = [ {'generated_text': [
+                    {'role': 'user',       'content': '...'}, 
+                    {'role': 'assistant',  'content': "..."}
+                ] } ]
+"""
+
+response = chat_history[0]['generated_text'][-1]['content']
+
+print("--------------------------------------", response, "--------------------------------------", sep="\n")
+
