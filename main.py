13f002399f0b994a1cfc58b865b442c028861b72
elichiara <chiarottoelisa@gmail.com>
2026-01-06 10:50:25 +0100

    decomposing docs into chunks

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # libraries
 import torch
 from transformers import pipeline # pipeline is for inference
+from langchain import embeddings
 from langchain_community.document_loaders import DirectoryLoader, TextLoader
-# note: langchain-community is a separate module, not included in langchain
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 # ---------------------------------------------------------------------------------------------- #
 # check if I have an Nvidia GPU on the machine
@@ -15,7 +16,7 @@ model = pipeline("text-generation", model="Qwen/Qwen2.5-Coder-1.5B-Instruct")
 
 #"""
 # ---------------------------------------------------------------------------------------------- #
-# creating the database (without PDFs because they generate errors!)
+# creating the database (remove PDFs because they generate errors!)
 loader = DirectoryLoader(
     "./test-code/",
     loader_cls=TextLoader,
@@ -25,9 +26,16 @@ loader = DirectoryLoader(
 )
 docs = loader.load()
 print("n docs =", len(docs)) # just to check that all docs have been loaded correctly
-# there should be 7 files (without the pdf)
-#"""
+# there should be 7 files
 
+# splitting the docs into chunks
+splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+chunks = splitter.split_documents(docs)
+print("n chunks =", len(chunks))
+
+db_dir = "test-db"
+
+#"""
 # ---------------------------------------------------------------------------------------------- #
 user_prompt = "Can you copy-paste the code I used for the velocity-Verlet method?"
 messages = [
