4e60acafbee9adc8dd365ffee252fed9aef36615
elichiara <chiarottoelisa@gmail.com>
2026-03-23 11:32:57 +0100

    not printing tool output

--- src/model.py
+++ src/model.py
@@ -225,7 +225,7 @@ class Model():
                         else:
                         #"""
                             tool_result = dispatch_tool(self.tools, tool_name, tool_request["arguments"])
-                            if verbose > 2: print(">> tool result:", tool_result)
+                            #if verbose > 2: print(">> tool result:", tool_result)
 
                         # save the results to pass them to the model
                         tool_result = {
